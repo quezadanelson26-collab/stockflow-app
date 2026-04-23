ab7e22665ada1b5e061fda61d4437119874da5f7
quezadanelson26-collab <quezadanelson26@gmail.com>
2026-04-23 01:23:58 -0400

    Remove poLineItemSchema definition

--- schemas/poLineItems.ts
+++ schemas/poLineItems.ts
@@ -1,8 +1,1 @@
-import { z } from 'zod';
-
-export const poLineItemSchema = z.object({
-  product_id: z.string().uuid(),
-  variant_id: z.string().uuid().nullable(),
-  ordered_qty: z.number().int().positive(),
-  cost: z.number().nonnegative(),
-});
+export {};
